994628269c72a3d313e61d9d5f263328e611d346
Vladislav Samoilenko <79825223592@yandex.ru>
2022-04-08 16:23:35 +0300

    changed properties, added annotation "Key"

--- src/test/java/config/ProjectConfig.java
+++ src/test/java/config/ProjectConfig.java
@@ -9,15 +9,19 @@ import org.aeonbits.owner.Config;
 })
 public interface ProjectConfig extends Config {
 
+    @Key("browser")
     @DefaultValue("chrome")
     String browser();
 
+    @Key("browserVersion")
     @DefaultValue("91.0")
     String browserVersion();
 
+    @Key("browserSize")
     @DefaultValue("1920x1080")
     String browserSize();
 
+    @Key("remoteDriverUrl")
     @DefaultValue("")
     String remoteDriverUrl();
 }
